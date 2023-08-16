840a164c87c434f69e54dff2f931abf7981b2129
Daniel Tan <dtch1997@users.noreply.github.com>
2023-08-16 17:33:25 +0100

    Register SVF envs

--- svf_gymnasium/train.py
+++ svf_gymnasium/train.py
@@ -14,6 +14,7 @@ from stable_baselines3.common.utils import set_random_seed
 
 # Register custom envs
 import rl_zoo3.import_envs  # noqa: F401 pytype: disable=import-error
+import svf_gymnasium.envs  # noqa: F401 pytype: disable=import-error
 from rl_zoo3.exp_manager import ExperimentManager
 from rl_zoo3.utils import ALGOS, StoreDict
 
