26525f9d4fa6da9fd17209093189957312a13858
Daniel Tan <dtch1997@users.noreply.github.com>
2023-08-18 11:48:37 +0100

    Switch to Lightning run

--- svf_gymnasium/experiments/run.sh
+++ svf_gymnasium/experiments/run.sh
@@ -7,12 +7,13 @@ do
     do 
         for robot in Point Car # Doggo
         do
-            for level in 0 1 2 
+            for level in 1 2
             do 
                 for algo in ppo sac dreamer_v3 p2e_dv2
                 do
                     env_id="Wrapped-Safety${robot}${task}${level}-v0"
-                    python -m svf_gymnasium.sheeprl.train ${algo} \
+                    lightning run model --accelerator=gpu --strategy=ddp --devices=1 \
+                        svf_gymnasium/sheeprl/train.py ${algo} \
                         --env $env_id \
                         --seed $seed \
                         --track \
